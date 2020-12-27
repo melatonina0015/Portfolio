d78e1cd4ccc0732c5ce8cddaf46a140b108e515e
Amelia Kwiecinska <kwam0015@gmail.com>
2020-12-27 20:38:02 +0100

    complete reshape

--- server.py
+++ server.py
@@ -1,8 +1,17 @@
-from flask import Flask, template_rendered
+from flask import Flask, render_template, url_for, request
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return render_template("index.html")
+
+@app.route('/submit_form', methods=['POST', 'GET'])
+def submit_form():
+    if request.method == 'POST':
+        data = request.form.to_dict()
+        print(data)
+        return "form submitted"
+    else:
+        return "error"
 
